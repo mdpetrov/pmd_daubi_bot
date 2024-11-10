00ea35663d35cd39b9e34a95648b83fd9caaec03
Max Petrov <mpetrov95@gmail.com>
2024-11-10 16:20:16 +0100

    fix wrong module was called in phrase_operations

--- pmd_daubi_bot/phrase_operation.py
+++ pmd_daubi_bot/phrase_operation.py
@@ -1,30 +1,30 @@
 import pandas as pd
-from pmd_daubi_bot.bot_operation import BotOperations
+from pmd_daubi_bot.log_operation import LogOperations
 
 
 class PhraseOperations(object):
-    def __init__(self, config, bot):
+    def __init__(self, config):
         self.config = config
-        self.BO = BotOperations(bot)
+        self.LO = LogOperations(config)
     def random_phrase(self, chat_id):
         path = self.config.path
-        BO = self.BO
-        BO.write_log(chat_id, ': Load phrases')
+        LO = self.LO
+        LO.write_log(chat_id, ': Load phrases')
         with open(path['text_phrases'], mode='rt', encoding='utf-8') as con:
             phrases = pd.read_csv(con, sep=';')
         phrase = phrases['phrase'].sample(n=1, weights=phrases['weight']).tolist()[0]
-        BO.write_log(chat_id, f'{phrase}')
+        LO.write_log(chat_id, f'{phrase}')
         return phrase
     
     def random_readycheck_phrase(self, chat_id):
         path = self.config.path
-        BO = self.BO
+        LO = self.LO
         with open(path['readycheck_phrases'], mode='rt', encoding='utf-8') as con:
-            BO.write_log(chat_id, ': Load phrases')
+            LO.write_log(chat_id, ': Load phrases')
             phrases = pd.read_csv(con, sep=';')
         phrases = phrases.query(f'chat_id == {chat_id}')
         phrase = phrases['phrase'].sample(n=1, weights=phrases['weight']).tolist()[0]
-        BO.write_log(chat_id, f'{phrase}')
+        LO.write_log(chat_id, f'{phrase}')
         return phrase
 
     
