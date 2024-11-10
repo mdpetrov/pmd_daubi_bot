2e172c5e11027e10a35f86e2fc48a7a523071c94
Max Petrov <mpetrov95@gmail.com>
2024-11-10 16:26:46 +0100

    add default readycheck_phrase

--- pmd_daubi_bot/config/config.py
+++ pmd_daubi_bot/config/config.py
@@ -3,4 +3,5 @@ path = {"text_phrases": "./.secret/input/text_phrases.csv",
         "log_dir": "./.secret/log", 
         "data_dir": "./.secret/data",
         "readycheck_phrases": "./.secret/input/readycheck_phrases.csv"}
-param_value = {'readycheck_cd': 60 * 60}
+param_value = {'readycheck_cd': 60 * 60,
+               'readycheck_default_phrase': 'Объявите время гейминга!'}
--- pmd_daubi_bot/phrase_operation.py
+++ pmd_daubi_bot/phrase_operation.py
@@ -19,10 +19,14 @@ class PhraseOperations(object):
     def random_readycheck_phrase(self, chat_id):
         path = self.config.path
         LO = self.LO
+        param_value = self.config.param_value
         with open(path['readycheck_phrases'], mode='rt', encoding='utf-8') as con:
             LO.write_log(chat_id, ': Load phrases')
             phrases = pd.read_csv(con, sep=';')
         phrases = phrases.query(f'chat_id == {chat_id}')
+        if len(phrases) == 0: #no phrase for this chat, putting the default one
+            LO.write_log(chat_id, 'Phrase list for the chat not found; putting the default one')
+            return param_value['readycheck_default_phrase']
         phrase = phrases['phrase'].sample(n=1, weights=phrases['weight']).tolist()[0]
         LO.write_log(chat_id, f'{phrase}')
         return phrase
