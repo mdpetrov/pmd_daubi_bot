408048602821c0f4ad8cf6c957efb85910106971
Max Petrov <mpetrov95@gmail.com>
2025-06-13 18:23:24 +0200

    Remove python version when setting up venv

--- bin/create_venv.sh
+++ bin/create_venv.sh
@@ -2,7 +2,7 @@
 
 prog_path=/usr/bot/pmd_daubi_bot
 
-sudo apt install python3.10-venv
+sudo apt install python3-venv
 python3 -m venv $prog_path/venv
 source $prog_path/venv/bin/activate
 pip install -r $prog_path/requirements.txt 
